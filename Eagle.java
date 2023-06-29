214698d07cfebbdc6da0270850994aa6dada3449
KevinRoig <kevinroig@hotmail.com>
2023-06-29 17:18:41 +0200

    answer

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,47 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.println(this.getName() + " takes off in the sky");
+        }
+    }
+
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 1000);
+            System.out.println(this.getName() + " is flying upward, altitude: " + this.altitude);
+        }
+        return this.altitude;
+    }
+
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters, 10);
+            System.out.println(this.getName() + " is flying down, altitude " + this.altitude);
+        }
+        return this.altitude;
+
+    }
+
+    @Override
+    public void glide() {
+        if (this.altitude == 150) {
+            System.out.println(this.getName() + " is gliding");
+        }
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude > 1 && this.flying) {
+            System.out.println(this.getName() + " is too high, can not land");
+        } else if (this.altitude == 1 && this.flying) {
+            this.flying = false;
+            System.out.println(this.getName()+ " lands on the ground");
+        }
     }
 }
